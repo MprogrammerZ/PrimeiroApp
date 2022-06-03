c12ab1bfa9465bee2ee153ba820685a8d5f9ce5c
marci <marcio.av.zanoni@gmail.com>
2022-06-03 12:16:14 -0400

    First Program Kotlin

--- app/src/main/java/com/marcio/primeiroapp/dice.kt
+++ app/src/main/java/com/marcio/primeiroapp/dice.kt
@@ -1,22 +1,39 @@
-
-
 package com.marcio.primeiroapp
 
-import java.lang.Exception
+fun main() {
 
-fun main(){
+    data class Input(val numero1: Int, val numero2: Int)
 
-    println("First enter Marcio's Number, Second enter Wendy's Number")
 
-    val marcio = readLine()?: "0"
-    val parsedNumber1 = try {
-        marcio.toInt()
-    }catch(e: Exception){ 0 ; println("That's not a valid number")  }
+    fun getInput(): Input {
+        println("First enter Marcio's Number, Second enter Wendy's Number")
 
-    val wendy = readLine()?: "0"
-    val parsedNumber2 = try {
-        wendy.toInt()
-    }catch(e: Exception){ 0 ; println("That's not a valid number") }
+        val marcio = readLine() ?: "0"
+        val parsedNumber1 = try {
+            marcio.toInt()
+        } catch (e: Exception) {
+            println("That's not a valid number")
+            0
+        }
+
+        val wendy = readLine() ?: "0"
+        val parsedNumber2 = try {
+            wendy.toInt()
+        } catch (e: Exception) {
+            println("That's not a valid number")
+            0
+        }
+
+        val input: Input = if (parsedNumber1 == 0 || parsedNumber2 == 0) {
+            getInput()
+        } else {
+            Input(parsedNumber1, parsedNumber2)
+        }
+
+        return input
+    }
+
+    val values = getInput()
 
     val firstDice = Dice(6)
     val secondDice = Dice(6)
@@ -24,15 +41,22 @@ fun main(){
     val answer1 = firstDice.roll()
     val answer2 = secondDice.roll()
 
-    if (parsedNumber1 == answer1){
-        println("Marcio you got it!")}else{println("Marcio you failed!")}
-    if (parsedNumber2 == answer2){
-        println("Wendy you got it!")}else{println("Wendy you failed!")}
+    if (values.numero1 == answer1) {
+        println("Marcio you got it!")
+    } else {
+        println("Marcio you failed!")
+    }
+    if (values.numero2 == answer2) {
+        println("Wendy you got it!")
+    } else {
+        println("Wendy you failed!")
+    }
 }
 
-open class Dice(var sides: Int ) {
+open class Dice(var sides: Int) {
     fun roll(): Int {
         val result = (1..sides).random()
         println("$result")
         return result
-    } }
+    }
+}
